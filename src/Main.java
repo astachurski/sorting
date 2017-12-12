2cc59c6ef48b4856f5aab69292f9bca267b2b205
astachurski <astachurski@gmail.com>
2017-12-12 20:30:12 +0100

    some change.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         //rstenr
+        //some other change
+        //rsturt this is from dev
     }
 }
